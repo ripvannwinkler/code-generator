202a55aeb0c10bf8a726f4b0c5a30a91026bdb5c
Chris Vann <chris@elserinteractive.com>
2017-10-14 10:32:47 -0500

    add help message

--- codes.js
+++ codes.js
@@ -1,37 +1,63 @@
-#!/usr/bin/env node
-"use strict";
-
-const minimist = require("minimist")
+const minimist = require("minimist");
 const defaultAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 const generateCode = (length, alphabet) => {
-    let a = "";
-    for (var i=0; i<length; i++) {
-        const rnd = Math.random()*1000000;
-        const pos = Math.floor(rnd % alphabet.length);
-        a += alphabet[pos];
-    } return a;
-}
+  let a = "";
+  for (var i = 0; i < length; i++) {
+    const rnd = Math.random() * 1000000;
+    const pos = Math.floor(rnd % alphabet.length);
+    a += alphabet[pos];
+  }
+  return a;
+};
+
+const printHelp = () => {
+  console.log("Usage: node codes.js [options]");
+  console.log("");
+  console.log("Options:");
+  console.log("  -c, --count        number of codes to generate");
+  console.log("  -l, --length       character length of each code");
+  console.log("  -p, --prefix       prefix each code with this");
+  console.log("                     (total length = length - prefix)");
+  console.log("  -a, --alphabet     list of characters/digits to use in codes");
+  console.log("                     (e.g. -a ABCDEFG123");
+  console.log("");
+  console.log("Examples:");
+  console.log("");
+  console.log("  node codes.js -c 50000");
+  console.log("    (50,000 codes, default length w/ no prefix)");
+  console.log("");
+  console.log("  node codes.js -l 8 -c 200 -p AB -a 1234567890");
+  console.log("    (200 codes, 8 characters each, numeric only)");
+};
 
 const argv = minimist(process.argv.slice(2), {
-	default: {
-		c: 10, 
-		a: defaultAlphabet,
-		p: "",
-		l: 10
-	}
+  default: {
+    c: 0,
+    a: defaultAlphabet,
+    p: "",
+    l: 10,
+    h: false
+  },
+  boolean: ["h", "help"]
 });
 
-console.dir(argv);
+const showHelp = argv.help || argv.h;
 
-const count = argv.count || argv.c;
-const alphabet = argv.alphabet || argv.a;
-const prefix = argv.prefix || argv.p;
-const length = argv.length || argv.l;
+if (showHelp) {
+  printHelp();
+} else {
+  const count = argv.count || argv.c;
+  const alphabet = argv.alphabet || argv.a;
+  const prefix = argv.prefix || argv.p;
+  const length = argv.length || argv.l;
 
-console.log(count, alphabet, prefix, length);
-
-for (var i = 0; i < count; i++) {
-    const code = generateCode(length, alphabet);
-    console.log(prefix + code);
+  if (count > 0) {
+    for (var i = 0; i < count; i++) {
+      const code = generateCode(length, alphabet);
+      console.log(prefix + code);
+    }
+  } else {
+    console.log("Nothing to do. Try --help");
+  }
 }
